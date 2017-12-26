f4c1e091a880a46a498834a236d95ec9ce6e8aae
momo <momoko.k.0421@gmail.com>
2017-12-27 00:01:18 +0900

    Add minor change to pick_and_placer.cpp

--- pick_and_placer/src/pick_and_placer.cpp
+++ pick_and_placer/src/pick_and_placer.cpp
@@ -11,7 +11,7 @@ int main(int argc, char **argv) {
 
   moveit::planning_interface::MoveGroupInterface arm("arm"); // set moveGroupInterface as arm ( this comes from moveit_setup_assistant)
   arm.setPoseReferenceFrame("base_link");
-  arm.setNamedTarget("vertical"); // set the arm "vertical" pose
+  arm.setNamedTarget("resting"); // set the arm "vertical" pose
   arm.move();
 
   ros::shutdown();
