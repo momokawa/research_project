4ebc72afe69c8e0d5d64ce752888b3c112f9234d
momo <momoko.k.0421@gmail.com>
2017-12-27 00:30:19 +0900

    Add change to pick_and_placer: move to the defined position

--- pick_and_placer/src/pick_and_placer.cpp
+++ pick_and_placer/src/pick_and_placer.cpp
@@ -11,9 +11,27 @@ int main(int argc, char **argv) {
 
   moveit::planning_interface::MoveGroupInterface arm("arm"); // set moveGroupInterface as arm ( this comes from moveit_setup_assistant)
   arm.setPoseReferenceFrame("base_link");
-  arm.setNamedTarget("resting"); // set the arm "vertical" pose
-  arm.move();
+  
+  // prepare
+  ROS_INFO("Moging to prepare pose");
+  geometry_msgs::PoseStamped pose;
+  pose.header.frame_id = "base_link";
+  pose.pose.position.x = 0.2;
+  pose.pose.position.y = 0.0;
+  pose.pose.position.z = 0.1;
+  pose.pose.orientation.x = 0.0;
+  pose.pose.orientation.y = 0.707106;
+  pose.pose.orientation.z = 0.0;
+  pose.pose.orientation.w = 0.707106;
 
+  // move to the defined position
+  // TODO change the position later
+  arm.setPoseTarget(pose);
+  if (!arm.move()) {
+    ROS_WARN("Could not move to prepare pose");
+    return 1;
+  }
+  
   ros::shutdown();
   return 0;
 }
