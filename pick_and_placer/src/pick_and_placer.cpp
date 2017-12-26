66dd313452eaaee8f8f9f911f0ae8cf113c407c6
momo <momoko.k.0421@gmail.com>
2017-12-26 16:55:50 +0900

    Recreate simple node and Cmake file

--- pick_and_placer/src/pick_and_placer.cpp
+++ pick_and_placer/src/pick_and_placer.cpp
@@ -1,8 +1,18 @@
 #include <ros/ros.h>
+#include <moveit/move_group_interface/move_group_interface.h>
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "pickandplacer");
+  ros::init(argc, argv, "pick_and_placer_pick_and_placer");
   ros::NodeHandle nh;
+
+  // for moveit's async calc
+  ros::AsyncSpinner spinner(2); // use 2 threads
+  spinner.start();
+
+  moveit::planning_interface::MoveGroupInterface arm("arm"); // set moveGroupInterface as arm ( this comes from moveit_setup_assistant)
+  arm.setPoseReferenceFrame("base_link");
+  arm.setNamedTarget("resting"); // set the arm "vertical" pose
+  arm.move();
 
   ros::shutdown();
   return 0;
