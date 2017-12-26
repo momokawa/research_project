9e06bdfcdd2eb2fd6ed5d9bcf92041b885f63972
momo <momoko.k.0421@gmail.com>
2017-12-27 00:57:06 +0900

    Use [resing] pose to move: this is just for testing

--- pick_and_placer/src/pick_and_placer.cpp
+++ pick_and_placer/src/pick_and_placer.cpp
@@ -12,25 +12,10 @@ int main(int argc, char **argv) {
   moveit::planning_interface::MoveGroupInterface arm("arm"); // set moveGroupInterface as arm ( this comes from moveit_setup_assistant)
   arm.setPoseReferenceFrame("base_link");
   
-  // prepare
-  ROS_INFO("Moging to prepare pose");
-  geometry_msgs::PoseStamped pose;
-  pose.header.frame_id = "base_link";
-  pose.pose.position.x = 0.2;
-  pose.pose.position.y = 0.0;
-  pose.pose.position.z = 0.1;
-  pose.pose.orientation.x = 0.0;
-  pose.pose.orientation.y = 0.707106;
-  pose.pose.orientation.z = 0.0;
-  pose.pose.orientation.w = 0.707106;
-
-  // move to the defined position
-  // TODO change the position later
-  arm.setPoseTarget(pose);
-  if (!arm.move()) {
-    ROS_WARN("Could not move to prepare pose");
-    return 1;
-  }
+  // to just for test
+  // get the each joint angle
+  arm.setNamedTarget("resting");
+  arm.move();
   
   ros::shutdown();
   return 0;
