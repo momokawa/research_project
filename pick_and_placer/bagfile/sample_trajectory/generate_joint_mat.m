cd0063d1939207347484199d6d133ee9530582ad
momo <momoko.k.0421@gmail.com>
2018-01-06 20:50:57 +0900

    Create mat file to generate required array to use in simlink simulation

---
+++ pick_and_placer/bagfile/sample_trajectory/generate_joint_mat.m
@@ -0,0 +1,11 @@
+% -----
+% this file creates mat file of each joint for using torque calculation
+% -----
+
+generate_joint_mat_func('bracket_1_2_joint.mat', 11);
+generate_joint_mat_func('bracket_2_3_joint.mat', 12);
+generate_joint_mat_func('bracket_3_4_joint.mat', 13);
+generate_joint_mat_func('bracket_4_5_joint.mat', 14);
+generate_joint_mat_func('bracket_5_6_joint.mat', 15);
+generate_joint_mat_func('bracket_6_7_joint.mat', 16);
+
---
+++ pick_and_placer/bagfile/sample_trajectory/generate_joint_mat_func.m
@@ -0,0 +1,18 @@
+function generate_joint_mat_func(mat_filename, raw_num)
+% this function generate mat_file of a joint with specific raw_num's data
+% mat_filename: bracket_1_2_joint.mat
+% raw_num: the num of raw where each joint array exists
+
+data_input = readtable('data.txt', 'Delimiter', 'comma');
+time_array = 0:0.01:0.01*1433; % 0.01sec interval array of time
+joint_position_table = data_input(:, raw_num); % get the joint's position arra
+joint_position_array = table2array(joint_position_table); % convert table to array
+
+% create mat file with required structs
+joint_position_trajectory.time = time_array';
+joint_position_trajectory.values = joint_position_array;
+joint_position_trajectory.dimension = 1;
+joint_position_trajectory.label = 'joint_position';
+save(mat_filename, 'joint_position_trajectory');
+
+end
